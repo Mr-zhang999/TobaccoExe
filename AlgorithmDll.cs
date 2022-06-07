f40996ca11bb6297e92105972d49b1ab90ccd643
Mr-zhang999 <1041872694@qq.com>
2022-06-07 10:30:29 +0800

    算法调用测试

---
+++ AlgorithmDll.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TobaccoExe
+{
+    class AlgorithmDll
+    {
+        [DllImport("fuzyyDll.dll", EntryPoint = "cacFuzzy", CallingConvention = CallingConvention.Cdecl)]
+        private static extern double cacFuzzy(double d1, double d2);
+        public double fuzzyDllCac(double d1, double d2)
+        {
+            double a = cacFuzzy(d1, d2);
+            return a;
+        }
+    }
+}
